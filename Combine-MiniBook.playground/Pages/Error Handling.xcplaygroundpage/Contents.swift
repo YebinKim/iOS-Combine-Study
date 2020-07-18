23442081510df02ecd6eecc88edc5b8d74427229
Mumani <yb2316@naver.com>
2020-07-19 07:31:55 +0900

    :pencil: Error Handling 공부 (2차 업데이트)

--- Combine-MiniBook.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Combine-MiniBook.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -13,8 +13,9 @@ import Combine
  ## Never
 
  - 실패하지 않음을 보장할 수 있는 Failure 타입
- - *setFailureType* : failure 타입이 Never인 Publisher에서만 사용되는 연산자, custom failure 타입을 지정할 수 있다. (단, Failure 타입은 Never 타입이어야 한다.)
- - *assign(to:on:)* : failure 타입이 Never인 Publisher에서만 사용되는 연산자, key path 중 오류가 있으면 결과값으로 unhandled error를 받거나 이상 동작 발생
+ - Failure 타입이 Never인 Publisher에서만 사용되는 연산자
+   - *setFailureType* : custom failure 타입을 지정할 수 있다. (단, Failure 타입은 Never 타입이어야 한다.)
+   - *assign(to:on:)* : key path 중 오류가 있으면 결과값으로 unhandled error를 받거나 이상 동작 발생
  - *assertNoFailure* : Swift의 *fatalError*와 비슷한 동작, Upstream에서 failure 이벤트를 완료할 수 없을 때 Debug Error 출력
  */
 
@@ -73,9 +74,78 @@ example(of: "assign") {
 example(of: "assertNoFailure") {
     Just("Hello")
         .setFailureType(to: MyError.self)
-        .tryMap { _ in throw MyError.ohNo } // 오류 발생
+//        .tryMap { _ in throw MyError.ohNo } // 오류 발생
         .assertNoFailure()
         .sink(receiveValue: { print("Got value: \($0) ")})
+        .store(in: &subscriptions)
+}
+
+
+/*:
+ ## Dealing with failure
+
+ - 실패 가능한 Failure 타입의 이벤트를 처리하는 방법
+ - try 계열 operators
+   - 실패 가능한 Failure 타입을 가지는 Operator
+   - failure 이벤트가 발생했을 때 completion block에서 처리할 수 있다.
+   - **Subscription의 Custom Error 타입을 Swift 일반 Error 타입으로 변경시킨다.**
+     - -> Custom Error 타입 처리 방법: Mapping errors
+   - *tryMap(_:)* : 오류를 throw 할 수 있는 map 연산자
+ - Mapping errors
+   - *mapError(_:)* : mapError는 Upstream Publisher로부터 발생한 오류를 Custom Error 타입으로 매핑시킨다.
+ */
+
+example(of: "tryMap") {
+
+    enum NameError: Error {
+        case tooShort(String)
+        case unknown
+    }
+
+    let names = ["Scott", "Marin", "Shai", "Florent"].publisher
+
+    names
+//        .map { value -> Int in    // map은 오류를 throw 할 수 없는 method기 때문에 (_) throws -> _ 을 수행할 수 없다.
+        .tryMap { value -> Int in
+            let length = value.count
+
+            guard length >= 5 else {
+                throw NameError.tooShort(value)
+            }
+
+            return value.count
+    }
+    .sink(receiveCompletion: {
+        print("Completed with \($0)") },
+          receiveValue: { print("Got value: \($0)") })
+}
+
+example(of: "map vs tryMap") {
+
+    enum NameError: Error {
+        case tooShort(String)
+        case unknown
+    }
+
+    Just("Hello")
+        .setFailureType(to: NameError.self)
+//        .map { $0 + " World!" }
+        .tryMap { $0 + " World!" }
+//        .tryMap { throw NameError.tooShort($0) }  // 오류 발생
+        .mapError { $0 as? NameError ?? .unknown }
+        .sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    print("Done!")
+                case .failure(.tooShort(let name)):
+                    print("\(name) is too short!")
+                case .failure(.unknown):
+                    print("An unknown name error occurred")
+                }
+        },
+            receiveValue: { print("Got value \($0)") }
+    )
         .store(in: &subscriptions)
 }
 
